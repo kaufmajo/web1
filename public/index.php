54077290d08988bacf4d4a2a18d398f1f09a1f6b
Joachim Kaufmann <joachim_kaufmann@bluewin.ch>
2025-03-20 16:19:55 +0100

    ...

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     return false;
 }
 
-ini_set ( "session.cookie_lifetime" , 5);
+//ini_set ( "session.cookie_lifetime" , 0);
 
 // -------------------------
 // start custom bootstrap code
